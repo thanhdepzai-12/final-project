b977f4eac45f76a4f02913b78fd4308a51d2c931
thanhdepzai-12 <102566654+thanhdepzai-12@users.noreply.github.com>
2022-06-19 19:51:05 +0700

    update

--- js/app.js
+++ js/app.js
@@ -30,5 +30,40 @@ btn.style.display="none";
     } );
 
 
+const photo = document.querySelector(".main-photo-s-4 ");
+const photo1 = document.querySelector(".photo-1");
+const photo2 = document.querySelector(".photo-2");
+const photo3 = document.querySelector(".photo-3");
 
+// console.log(photo , photo1 , photo2 , photo3)
 
+photo1.addEventListener("mouseover" , ()=>{
+   photo.style.display = "block";
+   photo2.style.display = "none";
+   photo3.style.display = "none";
+})
+photo1.addEventListener("mouseleave" , ()=>{
+    photo.style.display = "grid";
+    photo2.style.display = "block";
+    photo3.style.display = "block";
+ })
+ photo2.addEventListener("mouseover" , ()=>{
+    photo.style.display = "block";
+    photo1.style.display = "none";
+    photo3.style.display = "none";
+ })
+ photo2.addEventListener("mouseleave" , ()=>{
+     photo.style.display = "grid";
+     photo1.style.display = "block";
+     photo3.style.display = "block";
+  })
+  photo3.addEventListener("mouseover" , ()=>{
+    photo.style.display = "block";
+    photo1.style.display = "none";
+    photo2.style.display = "none";
+ })
+ photo3.addEventListener("mouseleave" , ()=>{
+     photo.style.display = "grid";
+     photo1.style.display = "block";
+     photo2.style.display = "block";
+  })
